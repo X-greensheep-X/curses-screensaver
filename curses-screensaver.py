e4536cb5ff29cc32021729caae1d3bc700f6367f
Brandon Vincent <37938084+X-greensheep-X@users.noreply.github.com>
2022-03-17 19:36:27 -0400

    add GPL comment

--- curses-screensaver.py
+++ curses-screensaver.py
@@ -2,6 +2,11 @@
 # Brandon Vincent 20220317
 # Uses python curses library to create a screensaver capable of reducing burn-in on LCD and possibly CRT terminals
 
+# GPLv2 statement
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 import curses
 import signal
 import time
